d9eea5b712a9c18c5791a0f3582bb79ae86de50a
DooMlll <76614788+DooMlll@users.noreply.github.com>
2022-12-09 21:58:06 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
+from .models import Post, Author, Category, User
+from datetime import datetime
+
+
+
+
+# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['categories'] = Category.objects.all()
+        context['authors'] = Author.objects.all()
+        context['form'] = PostForm()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'search'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
